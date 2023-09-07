e89649effbd5fb8e7ae43ed75e807be7fcea505f
Andrij <zinukandrij@gmail.com>
2023-09-07 14:10:24 +0300

    пусто

---
+++ lab_work1.cpp
@@ -0,0 +1,26 @@
+/*
+# Andrij Zyniuk IPZ 22k/1 | Variant - 3
+
+1. c = (a & b);
+2. (sq(a) + 2 * b * c + sq(c)) * PI / 2
+*/
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    float A, B, C;
+    const double PI = 3.14;
+
+    cout << "Please, enter A:";
+    cin >> A;
+    cout << "Please, enter B:";
+    cin >> B;
+    cout << "Please, enter C:";
+    cin >> C;
+
+    
+}
---
+++ print_number_entered_user.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+
+    cout << "Enter: ";
+    cin >> x;
+
+    cout << "123:" << x;
+    return 0;
+}
