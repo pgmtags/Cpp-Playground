2d98eb27633ed7aa3c191f6f425075c1625f360a
Andrij <zinukandrij@gmail.com>
2023-09-13 17:37:45 +0300

    Create Lab_work 2

---
+++ Lab's/2/lab_work2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    float A, B, F1 = 0, F2 = 0, F3;  
+    int C, VARIANT = 3;
+
+    cout << "Please, enter number A, B, and an integer number: ";
+    cin >> A >> B >> C;
+
+    if (C > VARIANT) { F1 = tanh(A); } 
+    else { F2 = sqrt(A); }
+    F3 = pow(F1, F2);
+    cout << "A: " << A << "\nB: " << B << "\nC: " << C << "\nVariant: " << VARIANT << "\nF1: " << F1 << "\nF2: " << F2 << "\nF3: " << F3 << endl;
+    _getch();
+}
