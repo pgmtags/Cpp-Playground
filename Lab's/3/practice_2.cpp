3b3d4ec1061e56a3785d21e903e7d6731e136631
Andrij Zyniuk <62100293+pgmtags@users.noreply.github.com>
2023-09-20 12:55:07 +0300

    Update practice_2.cpp

--- Lab's/3/practice_2.cpp
+++ Lab's/3/practice_2.cpp
@@ -1,31 +1,37 @@
-#include <iostream>
-#include <iomanip>
+//Зинюк Андрій Андрійович ІПЗ 22К\1, 3 Варіант
 
-using namespace std;
+#include <iostream> 
+#include <iomanip> 
+#include 
 
-long long binomialCoefficient(int n, int k) {
-    if (k == 0 || k == n)
-        return 1;
-    else
-        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
-}
+using namespace std; 
 
-void printPascalTriangle(int numRows) {
-    for (int i = 0; i < numRows; ++i) {
-        for (int j = 0; j <= i; ++j) {
-            long long coef = binomialCoefficient(i, j);
-            cout << setw(3) << coef << " ";
-        }
-        cout << endl;
-    }
-}
+long long binomialCoefficient(int n, int k) { 
+    if (k == 0 || k == n) 
+        return 1; 
+    else 
+        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k); 
+} 
 
-int main() {
-    int numRows;
-    cout << "Enter the number of rows for Pascal's Triangle: ";
-    cin >> numRows;
+void printPascalTriangle(int numRows) { 
+    for (int i = 0; i < numRows; ++i) { 
+        long long rowSum = 0; // Variable to store the sum of the row elements
+        for (int j = 0; j <= i; ++j) { 
+            long long coef = binomialCoefficient(i, j); 
+            rowSum += coef; // Accumulate the sum
+            cout << setw(3) << coef << " "; 
+        } 
+        cout << "   Sum: " << rowSum; // Print the sum of the row
+        cout << endl; 
+    } 
+} 
 
-    printPascalTriangle(numRows);
+int main() { 
+    int numRows; 
+    cout << "Enter the number of rows for Pascal's Triangle: "; 
+    cin >> numRows; 
+
+    printPascalTriangle(numRows); 
 
     return 0;
 }
