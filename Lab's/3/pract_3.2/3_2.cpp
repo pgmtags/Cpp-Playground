3a8b47052821b23df5dcabb426b600e70be69646
Andrij Zyniuk <62100293+pgmtags@users.noreply.github.com>
2023-09-27 17:22:59 +0300

    TRASHSH

--- Lab's/3/pract_3.2/3_2.cpp
+++ Lab's/3/pract_3.2/3_2.cpp
@@ -4,6 +4,27 @@
 
 using namespace std;
 
+double function_y(double x) {
+    return (x - sqrt(x) + 2);
+}
+
 int main() {
+    /*
+    за умовою (-10; 3), тому я просто виписав їх у a = -9, b = 2
+    */
+    double a = -9;  // Початок інтервалу
+    double b = 2;   // Кінець інтервалу
+    double h = 1.5;  // Крок
     
+
+    cout << "Initial formula: y(x) = x - sqrt(x) + 2" << endl;
+
+    // Обчислення та виведення значень функції для кожного кроку
+    for (double x = a; x <= b; x += h) {
+        double y = function_y(x);
+
+        cout << "y(" << x << ") = " << y << endl;
+    }
+
+    _getch();
 }
