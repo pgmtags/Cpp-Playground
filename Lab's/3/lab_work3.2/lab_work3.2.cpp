429af5a5c1996cba7838e62a58555cb726f94eef
Andrij Zyniuk <62100293+pgmtags@users.noreply.github.com>
2023-09-27 20:36:10 +0300

    Update lab_work3.2.cpp

--- Lab's/3/lab_work3.2/lab_work3.2.cpp
+++ Lab's/3/lab_work3.2/lab_work3.2.cpp
@@ -11,15 +11,13 @@ double function_y(double x) {
 int main() {
     // За умовою (-10; 3), тому я просто виписав їх у a = -9, b = 2
     double a = -9, b = 2, h = 1.5, x = a; 
-
     cout << "Initial formula: y(x) = x - sqrt(x) + 2" << endl;
-
+    
     // Обчислення та виведення значень функції для кожного кроку
     while (x <= b) {
         double y = function_y(x);
         cout << "y(" << x << ") = " << y << endl;
         x += h;  // Збільшення x на крок h
     }
-
     _getch();
 }
