22188800545116f0f0a7ec2f8cb51b0cebf994ee
Andrij Zyniuk <62100293+pgmtags@users.noreply.github.com>
2023-09-27 19:02:38 +0300

    Update 3_2.cpp

--- Lab's/3/pract_3.2/3_2.cpp
+++ Lab's/3/pract_3.2/3_2.cpp
@@ -9,13 +9,8 @@ double function_y(double x) {
 }
 
 int main() {
-    /*
-    за умовою (-10; 3), тому я просто виписав їх у a = -9, b = 2
-    */
-    double a = -9;  // Початок інтервалу
-    double b = 2;   // Кінець інтервалу
-    double h = 1.5;  // Крок
-    double x = a;
+    // За умовою (-10; 3), тому я просто виписав їх у a = -9, b = 2
+    double a = -9, b = 2, h = 1.5, x = a; 
 
     cout << "Initial formula: y(x) = x - sqrt(x) + 2" << endl;
 
